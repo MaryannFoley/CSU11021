214e12973b0ba05bed0e05ec0147de8b00575572
Maryann Foley <maryann.v.foley@gmail.com>
2019-11-15 09:19:20 +0000

    Update Lab-3-5-Mode.s

--- Lab-3-5-Mode.s
+++ Lab-3-5-Mode.s
@@ -6,14 +6,53 @@
 	AREA	RESET, CODE, READONLY
 	ENTRY
 
+	LDR R8, =0
+	LDR R9, =1
 	LDR	R4, =tstN	; load address of tstN
 	LDR	R1, [R4]	; load value of tstN
 
 	LDR	R2, =tstvals	; load address of numbers
+
+load
+	LDR R5, [R2]
+	LDR R6, =0
+	LDR R7, =0
+	LDR R2, =tstvals
+	B loop
+
+main
+	CMP R8, R1
+	BEQ end
+	LDR R2, =tstvals
+	LDR R6, =4
+	MUL R7, R6, R8
+	ADD R2, R2, R7
+	LDR R5, [R2]
+	CMP R5, R0
+	BEQ next
+	B load
+
+loop
+	LDR R3, [R2]
+	CMP R5, R3
+	BNE after
+	ADD R6, R6, #1
+after
+	ADD R2, R2, #4
+	ADD R7, R7, #1
+	CMP R7, R1
+	BEQ next
+	B repeat
+
+next
+	ADD R8, R8, #1
+	CMP R9, R6
+	BHI main
+	MOV R0, R5
+	MOV R9, R6
+	B start
+end
 	
-	;
-	; Write your progam here to compute the mode in R0
-	;
 
 STOP	B	STOP
 
